0696b53ae3a7f1de3db29cd47bd4f95cc2dc7ceb
iam-arjunn <arjun.n.social.media@gmail.com>
2025-12-26 21:23:47 +0530

    Added More Testing

--- Unit Testing/basic-testing-frontend/src/math.test.js
+++ Unit Testing/basic-testing-frontend/src/math.test.js
@@ -3,12 +3,36 @@ import {add} from'./math';
 
 it('Should summarize all number values in an array', () => {
     // Arrange
-    const numbers = [1, 2, 3, 4, 5];
+    const numbers = [1, 2, 3];
 
     // Act
     const result = add(numbers);
 
     // Assert
     const expectedResult = numbers.reduce((sum, num) => sum + num, 0);
+    expect(result).toBe(expectedResult);
+});
+
+it('Should yield NaN if  a non-number is present in the array', () => {
+    // Arrange
+    const numbers = [1, 'invalid'];
+
+    // Act
+    const result = add(numbers);
+
+    // Assert
+    expect(result).toBeNaN();
+});
+
+it('should yield a correct sum if an array of numeric string values is provided', () => {
+
+    // Arrange
+    const numbers = ['1', '2'];
+
+    // Act
+    const result = add(numbers);
+
+    // Assert
+    const expectedResult = numbers.reduce((sum, num) => sum + Number(num), 0);
     expect(result).toBe(expectedResult);
 });
